041b67f12870f3a4b27217a2fe688c0afd1fc4b1
Nelke Lee <databslee@gmail.com>
2022-11-27 23:11:16 +0900

    12장 코드 추가

---
+++ 02.effective_python/ep_12.py
@@ -0,0 +1,7 @@
+name_list = ['a', 'b', 'c', 'd', 1, 2, 3, 4]
+print(name_list[::]) #abcd1234
+print(name_list[::3]) #ad3
+print(name_list[1::]) #bcd1234
+print(name_list[:1:]) #a
+print(name_list[:1:3]) #a
+print(name_list[3:1:]) #
